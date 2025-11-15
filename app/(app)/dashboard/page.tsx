a0f0f4b80ececcb9dce03ef0e89c076b23043637
Prince Amanor Kabutey <facultywave124@gmail.com>
2025-11-14 21:31:48 -0800

    Move MobileDock to layout and remove from dashboard
    MobileDock component and its icons array were moved from the dashboard page to the app layout, ensuring consistent rendering across all pages. This refactor improves maintainability and UI consistency.

--- app/(app)/dashboard/page.tsx
+++ app/(app)/dashboard/page.tsx
@@ -1,21 +1,14 @@
 import CompanionCard from "@/components/ui/companionCard";
-import MobileDock from "@/components/ui/mobileDock";
+
 import { mockData } from "@/mock/data";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
-
-const icons = [
-  { src: "https://cdn.lordicon.com/pgirtdfe.json", id: 1, href: "/dashboard" },
-  { src: "https://cdn.lordicon.com/ytklkgsc.json", id: 2, href: "/pricing" },
-  { src: "https://cdn.lordicon.com/ueoydrft.json", id: 3, href: "/new" },
-];
 
 export default async function Dashboard() {
   const { userId } = await auth();
   if (!userId) redirect("/login");
   return (
     <main className="mt-15 px-4 sm:px-6 md:px-10">
-      <MobileDock icons={icons} />
       <div
         className="
     grid
--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,5 +1,11 @@
+import MobileDock from "@/components/ui/mobileDock";
 import Navbar from "@/components/ui/navbar";
 import Sidebar from "@/components/ui/sidebar";
+const icons = [
+  { src: "https://cdn.lordicon.com/pgirtdfe.json", id: 1, href: "/dashboard" },
+  { src: "https://cdn.lordicon.com/ytklkgsc.json", id: 2, href: "/pricing" },
+  { src: "https://cdn.lordicon.com/ueoydrft.json", id: 3, href: "/new" },
+];
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -7,7 +13,12 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
       <Sidebar />
       <div className="flex flex-col flex-1 overflow-y-auto scrollbar-hide ">
         <Navbar />
-        <main>{children}</main>
+        <main>
+          {children}
+          <div>
+            <MobileDock icons={icons} />
+          </div>
+        </main>
       </div>
     </div>
   );
