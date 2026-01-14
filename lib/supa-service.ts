e059f2248cfd1610f2a0304fbd9b983cd3d64ec4
Prince Amanor Kabutey <facultywave124@gmail.com>
2026-01-14 09:08:45 -0800

    Update supa-service.ts

--- lib/supa-service.ts
+++ lib/supa-service.ts
@@ -1,17 +1,21 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-// Use your custom SECRET key name here
-const supabaseSecretKey = process.env.SUPABASE_SECRET_KEY;
+export const getSupabaseService = () => {
+  const url = process.env.NEXT_PUBLIC_SUPABASE_URL;
+  const key = process.env.SUPABASE_SECRET_KEY;
 
-if (!supabaseUrl || !supabaseSecretKey) {
-  console.error("Supabase Service Key or URL is missing in Production!");
-}
+  if (!url || !key) {
+    throw new Error(
+      "Supabase Service Key is missing from Environment Variables"
+    );
+  }
 
-// This client is for BACKGROUND tasks like webhooks
-export const supabase = createClient(supabaseUrl!, supabaseSecretKey!, {
-  auth: {
-    autoRefreshToken: false,
-    persistSession: false,
-  },
-});
+  return createClient(url, key, {
+    auth: {
+      persistSession: false,
+      autoRefreshToken: false,
+    },
+  });
+};
+
+export const supabase = getSupabaseService();
