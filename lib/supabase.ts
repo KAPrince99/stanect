06c38ca05a6bd18be51c5f95fbb5011f22a97e83
Prince Amanor Kabutey <facultywave124@gmail.com>
2026-01-15 13:41:53 -0800

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -5,14 +5,14 @@ import { createClient } from "@supabase/supabase-js";
 export const createSupabaseClient = () => {
   const url = process.env.NEXT_PUBLIC_SUPABASE_URL!;
   const key = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!;
-  if (process.env.NODE_ENV === "production") {
-    console.log("--- Supabase Env Check ---");
-    console.log("URL exists:", !!url);
-    console.log("Key Name Used: NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY");
-    console.log("Key Prefix:", key ? key.slice(0, 14) : "UNDEFINED");
-    console.log("Key Length:", key ? key.length : 0);
-    console.log("--------------------------");
-  }
+  // if (process.env.NODE_ENV === "production") {
+  //   console.log("--- Supabase Env Check ---");
+  //   console.log("URL exists:", !!url);
+  //   console.log("Key Name Used: NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY");
+  //   console.log("Key Prefix:", key ? key.slice(0, 14) : "UNDEFINED");
+  //   console.log("Key Length:", key ? key.length : 0);
+  //   console.log("--------------------------");
+  // }
 
   if (!url || !key) {
     throw new Error(
