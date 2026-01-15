46659f5b92b3ee38b6a4a5ccd25945a262bffb2c
Prince Amanor Kabutey <facultywave124@gmail.com>
2026-01-15 15:15:46 -0800

    Added suspense fallbacks to both login and signUp pages

--- app/(auth)/login/[[...login]]/page.tsx
+++ app/(auth)/login/[[...login]]/page.tsx
@@ -1,5 +1,7 @@
+import LoadingSpinner from "@/components/ui/LoadingSpinner";
 import LoginComponent from "@/components/ui/LoginComponent";
 import { Metadata } from "next";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "Login – Stanect AI",
@@ -14,7 +16,9 @@ export const metadata: Metadata = {
 export default function LoginPage() {
   return (
     <main className="flex items-center justify-center min-h-screen  py-12">
-      <LoginComponent />
+      <Suspense fallback={<LoadingSpinner />}>
+        <LoginComponent />
+      </Suspense>
     </main>
   );
 }
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,7 @@
+import LoadingSpinner from "@/components/ui/LoadingSpinner";
 import SignupComponent from "@/components/ui/SignupComponent";
 import { Metadata } from "next";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "Sign Up – Stanect AI",
@@ -14,7 +16,9 @@ export const metadata: Metadata = {
 export default function SignUpPage() {
   return (
     <main className="flex items-center justify-center min-h-screen px-6 py-12">
-      <SignupComponent />
+      <Suspense fallback={<LoadingSpinner />}>
+        <SignupComponent />
+      </Suspense>
     </main>
   );
 }
