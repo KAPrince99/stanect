beba209913d1a19c10f8763a64d5e25321275cfb
Prince Amanor Kabutey <facultywave124@gmail.com>
2026-01-16 07:07:53 -0800

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,4 +3,7 @@ module.exports = {
   siteUrl: "https://www.stanect.com",
   generateRobotsTxt: true,
   sitemapSize: 5000,
+  changefreq: "daily",
+  priority: 0.7,
+  exclude: ["/dashbaord/*"],
 };
