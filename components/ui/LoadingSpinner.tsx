566e563247cfe4e019d937869a71014f2f9a2efb
Prince Amanor Kabutey <facultywave124@gmail.com>
2026-02-12 07:27:29 -0800

    Update LoadingSpinner.tsx

--- components/ui/LoadingSpinner.tsx
+++ components/ui/LoadingSpinner.tsx
@@ -5,9 +5,10 @@ export default function LoadingSpinner() {
   return (
     <div className="flex justify-center items-center my-50 lg:-ml-35">
       <LordIcon
-        src="https://cdn.lordicon.com/veoztjjj.json"
+        src="https://cdn.lordicon.com/flabvqvs.json"
         trigger="loop"
-        colors="primary:#e88c30,secondary:#ffffff"
+        state="loop-rotation-three-quarters"
+        colors="primary:#ffffff"
         height={60}
         width={60}
       />
