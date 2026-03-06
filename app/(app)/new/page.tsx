821808a8fdd44ddb6dd275df75ef4ef5809238ea
Prince Amanor Kabutey <facultywave124@gmail.com>
2026-03-06 14:31:42 +0000

    Update page.tsx

--- app/(app)/new/page.tsx
+++ app/(app)/new/page.tsx
@@ -1,5 +1,3 @@
-import LoadingSpinner from "@/components/ui/LoadingSpinner";
-import { Suspense } from "react";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import type { Metadata } from "next";
